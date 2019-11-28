e25e3e2bca72b01877bb7e9b72d6274cca9b76b3
nilesh <nilesh.kant@sakhatech.com>
2019-11-28 21:13:54 +0530

    added footer

--- src/components/Homepage/section5.jsx
+++ src/components/Homepage/section5.jsx
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cn from 'classnames';
+import VisibilitySensor from 'react-visibility-sensor';
 
 const Section5 = () => {
+  const [visible, setVisible] = useState(false);
+
+  const onVisibilitySensorChange = (isVisible) => {
+    if (isVisible) {
+      setVisible(true);
+    }
+  };
+
   return (
-    <div className="container py-5">
-      <div className="row">
-        <div className="col-md-8 offset-md-2 col-12 bg-white py-5 br-5 m-br-0">
-          <div className="text-center mont-600 pb-4">
-            Did you like my profile?
-            <span className="colorRed">
-              {' Say Hello At'}
-            </span>
-          </div>
-          <div className="text-center fs-30 mont-300 ls-3">
-            nileshkant10@gmail.com
+    <VisibilitySensor
+      onChange={onVisibilitySensorChange}
+    >
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-md-8 offset-md-2 col-12 bg-white py-5 br-5 m-br-0 text-center">
+            <div className="mont-600 pb-4">
+              Did you like my profile?
+              <span className="colorRed">
+                {' Say Hello At'}
+              </span>
+            </div>
+            <a
+              href={`mailto:nileshkant10@gmail.com?subject=${encodeURIComponent('I like your profile')}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="align-self-center colorVoilet"
+            >
+              <div className="d-inline-block">
+                <div className={cn('fs-30 m-fs-20 mont-300', visible ? 'typewriter' : 'd-none')}>
+                nileshkant10@gmail.com
+                </div>
+              </div>
+            </a>
           </div>
         </div>
       </div>
-    </div>
+    </VisibilitySensor>
   );
 };
 
